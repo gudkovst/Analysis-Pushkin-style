c075d689842977eace862cf60eb92cafd6157008
gudkovst <69453922+gudkovst@users.noreply.github.com>
2024-02-06 19:29:32 +0700

    add important for features

--- utils/dict_lib.py
+++ utils/dict_lib.py
@@ -1,10 +1,10 @@
 
-def dict_sort(d: dict, regime: int = 1) -> dict:
-    return dict(sorted(d.items(), key=lambda item: item[regime], reverse=True))
+def dict_sort(d: dict, regime: int = 1, reverse: bool = True) -> dict:
+    return dict(sorted(d.items(), key=lambda item: item[regime], reverse=reverse))
 
 
-def selection(d: dict, border: int) -> dict:
-    return dict(item for item in d.items() if item[1] > border)
+def selection(d: dict, down: float = float('-inf'), up: float = float('inf')) -> dict:
+    return dict(item for item in d.items() if up >= item[1] >= down)
 
 
 def agregate(f: dict, s: dict) -> dict:
@@ -13,12 +13,13 @@ def agregate(f: dict, s: dict) -> dict:
     return f
 
 
-def top_n(d: dict, n: int) -> dict:
-    d_sort = dict_sort(d)
+def top_n(d: dict, n: int, loser_reg: bool = False) -> dict:
+    d_sort = dict_sort(d, reverse=not loser_reg)
     vals = list(d_sort.values())
     index = n - 1 if n <= len(vals) else -1
-    bord = vals[index] - 1
-    return selection(d_sort, bord)
+    if loser_reg:
+        return selection(d_sort, up=vals[index])
+    return selection(d_sort, down=vals[index])
 
 
 def mean_key(d: dict, *args) -> float:
