b95b1569ad78aca67cbd4c099c561e16b5ad596c
gudkovst <69453922+gudkovst@users.noreply.github.com>
2024-03-18 17:56:29 +0700

    test ensemble

--- models/ensemble.py
+++ models/ensemble.py
@@ -1,9 +1,13 @@
 from models.data import *
-from periods import periods
+from periods import *
 from utils.text_feature import *
 from utils.ensemble_metadata import get
+from utils.show import confusion_matrix
 
 from keras import models
+from typing import TypeAlias
+
+conf_matrix: TypeAlias = None
 
 
 class TextData:
@@ -57,51 +61,113 @@ def predict_list(model_names: list[str], data: list[TextData]) -> None:
     for i, name in enumerate(model_names):
         model = models.load_model(root + f"models\\saved_models\\{name}.keras")
         d = [td.samples[i] for td in data]
-        print(len(d))
         p = model.predict(d)
         preds.append(p)
     for i, td in enumerate(data):
         prs = {}
         for j, pr in enumerate(preds):
-            prs[model_names[j]] = pr[i]  
+            prs[model_names[j]] = pr[i][0]
         td.predict_dict = prs
 
 
-def predict():
-    pass
+def get_classes_ticks(periods: list[period_type]) -> list[str]:
+    return [f'{period[0]} - {period[-1]}' for period in periods]
 
 
-def ensemble4():
-    models = ["model4", "model4-1"]
+def threshold(val: float, tr: float) -> int:
+    return 0 if val < tr else 1
+
+
+def pred2label(pred: int) -> int:
+    from random import shuffle, choice
+    a0 = [i for i in range(5)] + [6, 7, 13]
+    a01, a23 = [5, 8, 9, 10, 12], [18, 20, 28]
+    a3 = [17, 24, 25]
+    convs = {i: [0] for i in a0} | {i: [0, 1] for i in a01} | {i: [3] for i in a3} | {i: [2, 3] for i in a23}
+    convs |= {11: [0, 0, 1, 2], 14: [0, 0, 0, 1, 1], 15: [0]*4 + [1, 1, 2, 2, 3], 16: [2], 19: [0, 1, 2, 2, 3], 21: [0, 2, 3], 22: [0, 2, 2, 3, 3]}
+    convs |= {23: [0, 1] + [2, 3]*3, 26: [0, 1, 2, 3], 27: [0, 1] + [2, 3]*4, 29: [0, 1, 3] + [2, 3]*2, 30: [0, 0, 1] + [2, 3]*4, 31: [0, 1, 3] + [2, 3]*2}
+    pr = convs[pred]
+    shuffle(pr)
+    return choice(pr)
+
+
+def ensemble_thresholds_test(models: list[str], quorum: int) -> conf_matrix:
     fs = get(models, "features")
-    borders = get(models, "borders") 
+    borders = get(models, "borders")
     tr = get(models, "thresholds")
-    data = ensemble_test_data(fs, borders, 1)
+    data = ensemble_test_data(fs, borders, quorum)
+    print(f"size data = {len(data)}")
     predict_list(models, data)
-    preds = []
-    labels = []
+    labels = [td.label for td in data]
+    predicts = []
     for td in data:
         if not td.predicted:
             continue
-        pr4 = td.predict_dict["model4"]
-        pr4_1 = td.predict_dict["model4-1"]
-        if pr4 < tr[0] and pr4_1 < tr[1]:
-            labels.append(td.label)
-            preds.append(0)
-        elif pr4 > tr[0] and pr4_1 > tr[1]:
-            labels.append(td.label)
-            preds.append(3)
-        else:
-            labels.append(td.label)
-            preds.append(3)
-    tp = sum([l == preds[i] == 3 for i, l in enumerate(labels)])
-    fp = sum([l != preds[i] == 1 for i, l in enumerate(labels)])
-    tn = sum([l == preds[i] == 0 for i, l in enumerate(labels)])
-    fn = sum([l != preds[i] == 0 for i, l in enumerate(labels)])
-    print(tp, fp, tn, fn)
-    if tp + fp == 0:
-        return
-    print(f"precision = {tp / (tp + fp)}")
-    print(f'recall = {tp / (tp + fn)}')
-    print(f'fpr = {fp / (fp + tn)}')
-    print(f'acc = {(tp + tn) / (tp + tn + fp + fn)}')
+        pr = [td.predict_dict[model] for model in models]
+        pr_labels = [threshold(p, t) for p, t in zip(pr, tr)]
+        s = ''.join([str(p) for p in pr_labels])
+        predicts.append(pred2label(int(s, 2)))
+    return confusion_matrix(labels, predicts, get_classes_ticks(periods))
+
+
+def ensemble_thresholds_explore(models: list[str], quorum: int) -> list:
+    fs = get(models, "features")
+    borders = get(models, "borders")
+    tr = get(models, "thresholds")
+    data = ensemble_test_data(fs, borders, quorum)
+    print(f"size data = {len(data)}")
+    predict_list(models, data)
+    k = 2 ** len(models)
+    preds = [[] for i in range(k)]
+    for td in data:
+        if not td.predicted:
+            continue
+        pr = [td.predict_dict[model] for model in models]
+        pr_labels = [threshold(p, t) for p, t in zip(pr, tr)]
+        s = ''.join([str(p) for p in pr_labels])
+        preds[int(s, 2)].append(td.label)
+    return confusion_matrix(labels, preds, get_classes_ticks(periods))
+
+
+def add(base: list, addition: list) -> list:
+    assert len(base) == len(addition)
+    return [b + a for b, a in zip(base, addition)]
+
+
+def ensemble_parts_explore(models: list[str], quorum: int) -> confusion_matrix:
+    fs = get(models, "features")
+    borders = get(models, "borders")
+    tr = get(models, "thresholds")
+    data = ensemble_test_data(fs, borders, quorum)
+    print(f"size data = {len(data)}")
+    predict_list(models, data)
+    labels = [td.label for td in data]
+    predicts = []
+    for td in data:
+        if not td.predicted:
+            continue
+        preds = [0] * len(periods)
+        if "model1" in models:
+            p = td.predict_dict["model1"]
+            p01 = (1 - p) / 2
+            p23 = p / 2
+            preds = add(preds, [p01, p01, p23, p23])
+        if "model2" in models:
+            p = td.predict_dict["model2"]
+            p0 = 1 - p
+            p123 = p / 3
+            preds = add(preds, [p0, p123, p123, p123])
+        if "model4" in models or "model4-1" in models:
+            p = (td.predict_dict.get("model4", 0) + td.predict_dict.get("model4-1", 0)) / 2
+            p012 = p / 3
+            p3 = 1 - p
+            preds = add(preds, [p012, p012, p3, p3])
+        if "model5" in models:
+            p = td.predict_dict["model5"]
+            p1 = (1 - p) / 3
+            p2 = p / 3
+            preds = add(preds, [0, p1, p2, 0])
+        predicts.append(preds.index(max(preds)))
+    ticks = [f'{period[0]} - {period[-1]}' for period in periods]
+    return confusion_matrix(labels, predicts, ticks)
+        
