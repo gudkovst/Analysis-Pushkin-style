f7b513df7d0f0a57413f1ca6a23b5ab3e1260fa0
gudkovst <69453922+gudkovst@users.noreply.github.com>
2024-04-15 11:32:53 +0700

    refactoring

--- models/models.py
+++ models/models.py
@@ -1,14 +1,12 @@
 from keras import models
 from keras.layers import Dense
-from keras.regularizers import L2
 
 
-def get_model1(k: int) -> models:
+def get_dense_model(k_input: int, neurons: list[int]) -> models:
     model = models.Sequential()
-    model.add(Dense(16, activation='relu', input_shape=(k,)))
-    model.add(Dense(12, activation='silu'))
-    model.add(Dense(6, activation='silu'))
-    #model.add(Dense(4, activation='silu'))
+    model.add(Dense(neurons[0], activation='relu', input_shape=(k_input,)))
+    for neuron in neurons[1:]:
+        model.add(Dense(neuron, activation='silu'))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
     return model
